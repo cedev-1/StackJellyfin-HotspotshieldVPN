180ac52bfa44824e7e5051080781458bf3bb26a5
cedev-1 <121963179+cedev-1@users.noreply.github.com>
2025-03-10 21:02:10 +0100

    add -- folder script 📂

---
+++ folder.sh
@@ -0,0 +1,7 @@
+#bin/bash
+
+mkdir -p jellyfin radarr sonarr configs/qbittorrent configs/prowlarr configs/sonarr configs/radarr configs/jellyfin configs/jellyseerr
+echo "Config folders created"
+
+mkdir -p jellyfin radarr sonarr
+echo "Data folders created"
